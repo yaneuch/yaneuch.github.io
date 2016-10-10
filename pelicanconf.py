6de75922844f0176fea972939af2f5a635c9d355
yaneuch <babushkinkolobok@yandex.ru>
2016-10-10 23:05:11 +0300

    create a new Pelican-based website

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'\u0421\u0435\u0440\u0433\u0435\u0439 \u0411\u043e\u0439\u0447\u0435\u043d\u043a\u043e'
+SITENAME = u'"\u0412\u0435\u043a \u0443\u0447\u0438\u0441\u044c!"'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = u'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
